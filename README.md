5300b298e1653b3bff4695976fc9db1958ef14b7
victor <victor@pangea.io>
2022-10-27 12:00:05 +0700

    rename and add folder

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 
 ## Code a forecaster
+
--- src/config.py
+++ src/config.py
@@ -7,6 +7,5 @@ class Settings:
     root_dir: Path = Path(__file__).resolve().parents[1]
     storage_dir: Path = root_dir / "storage"
     datasets_dir: Path = root_dir / "datasets"
-    experiments_dir: Path = storage_dir / "exp_name"
 
 
